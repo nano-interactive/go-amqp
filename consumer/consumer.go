a5c53c1fc6cd1d708cab7631512334a234d7d17b
Dusan Malusev <dusan@dusanmalusev.dev>
2023-06-27 13:02:33 +0200

    Fixing nil deref

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -13,7 +13,7 @@ import (
 	"github.com/nano-interactive/go-amqp/v2/serializer"
 )
 
-var _ io.Closer = *(*Consumer[any])(nil)
+var _ io.Closer = Consumer[any]{}
 var _ io.Closer = (*Consumer[any])(nil)
 
 type (
